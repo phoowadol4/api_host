16254ceb7d348005d528ccc0153901a7d071cee8
phoowadol4 <puwadol18dux@gmail.com>
2024-07-24 12:01:33 +0700

    sa

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
