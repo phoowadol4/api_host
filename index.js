f7153c30de4407755db8c382d7584976d04d81da
phoowadol4 <puwadol18dux@gmail.com>
2024-07-24 12:22:31 +0700

    save

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const cors = require("cors");
 const app = express();
 
 const port = 3000;
-app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.use(cors());
+
+app.get("/", (req, res) => res.send("hello welcome Express on Vercel"));
+
+app.get("/step", (req, res) => res.send("hello welcome Express on Vercel page 2"));
+
+app.get("/step/inside", (req, res) => res.send("hello welcome Express on Vercel inside page 1"));
 
 app.listen(port, () => console.log(`Server ready on port ${port}`));
 
